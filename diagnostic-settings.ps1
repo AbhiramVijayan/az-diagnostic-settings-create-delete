61e0bcb02871d104cf54a69d3579d34465b6a555
AbhiramVijayan <abhiramvijayan1@gmail.com>
2021-09-28 14:26:56 +0530

    storage account added

--- diagnostic-settings.ps1
+++ diagnostic-settings.ps1
@@ -9,50 +9,53 @@ $Resources = Get-AzResource
 
 switch ($Number) {
   1 { 
-    $Name = Read-Host "Enter name eg: KeyVault-Diagnostics"
-    $Eventhub = Read-Host "Enter event hub name "
-    $resourceGroupname = Read-Host "Enter Resource group name"
+    $Name = "KeyVault-Diagnostics"   #Read-Host "Enter name eg: KeyVault-Diagnostics"
+    $Eventhub = "adfhub" #Read-Host "Enter event hub name "
+    $resourceGroupname = "deleteme"  #Read-Host "Enter Resource group name"
     foreach ($Resource in $Resources) {
       if ($Resource.Location -eq "eastus") {
-        # if ( $Resource.ResourceType -eq "Microsoft.Network/networkSecurityGroups" ) {
+        if ( $Resource.ResourceType -eq "Microsoft.Network/networkSecurityGroups" ) {
         
-        #   #for nsg
-        #   az monitor diagnostic-settings create `
-        #     --name $Name  `
-        #     --resource $Resource.ResourceId `
-        #     --logs '[{""category"": ""NetworkSecurityGroupEvent"",""enabled"": true},{""category"": ""NetworkSecurityGroupRuleCounter"",""enabled"": true}]' `
-        #     --event-hub-rule /subscriptions/$Subscriptionid/resourceGroups/$resourceGroupname/providers/Microsoft.EventHub/namespaces/$Eventhub/authorizationrules/RootManageSharedAccessKey
-      
-        # }
-
-        if ( $Resource.ResourceType -eq "Microsoft.Storage/storageAccounts") {
-        # $Resource.ResourceId + "/blob"
-          Write-Output  Resource.
+          #for nsg
           az monitor diagnostic-settings create `
             --name $Name  `
             --resource $Resource.ResourceId `
-            --metrics '[{""category"": ""Transaction"",""enabled"": true}]' `
+            --logs '[{""category"": ""NetworkSecurityGroupEvent"",""enabled"": true},{""category"": ""NetworkSecurityGroupRuleCounter"",""enabled"": true}]' `
             --event-hub-rule /subscriptions/$Subscriptionid/resourceGroups/$resourceGroupname/providers/Microsoft.EventHub/namespaces/$Eventhub/authorizationrules/RootManageSharedAccessKey
-
-          az monitor diagnostic-settings create `
-            --name $Name  `
-            --resource $blob `
-            --logs '[{""category"": ""StorageRead"",""enabled"": true},{""category"": ""StorageWrite"",""enabled"": true},{""category"": ""StorageDelete"",""enabled"": true}]' `
-            --metrics '[{""category"": ""Transaction"",""enabled"": true}]' `
-            --event-hub-rule /subscriptions/$Subscriptionid/resourceGroups/$resourceGroupname/providers/Microsoft.EventHub/namespaces/$Eventhub/authorizationrules/RootManageSharedAccessKey
+      
+        }
 
 
+        #for storage account 
+        if ( $Resource.ResourceType -eq "Microsoft.Storage/storageAccounts") {
+         
+        
+          $Ids = @($Resource.ResourceId + "/blobServices/default"
+            $Resource.ResourceId + "/fileServices/default"
+            $Resource.ResourceId + "/queueServices/default"
+            $Resource.ResourceId + "/tableServices/default"
+          )
+          $Ids | ForEach-Object {
+            az monitor diagnostic-settings create `
+              --name $Name  `
+              --resource $_ `
+              --logs '[{""category"": ""StorageRead"",""enabled"": true},{""category"": ""StorageWrite"",""enabled"": true},{""category"": ""StorageDelete"",""enabled"": true}]' `
+              --metrics '[{""category"": ""Transaction"",""enabled"": true}]' `
+              --event-hub-rule /subscriptions/$Subscriptionid/resourceGroups/$resourceGroupname/providers/Microsoft.EventHub/namespaces/$Eventhub/authorizationrules/RootManageSharedAccessKey
+
+          }
+      
 
         }
 
         else {
        
        
-          # az monitor diagnostic-settings create  `
-          #   --name  $Name `
-          #   --resource $Resource.ResourceId `
-          #   --metrics '[{""category"": ""AllMetrics"",""enabled"": true}]' `
-          #   --event-hub-rule /subscriptions/$Subscriptionid/resourceGroups/$resourceGroupname/providers/Microsoft.EventHub/namespaces/$Eventhub/authorizationrules/RootManageSharedAccessKey
+          az monitor diagnostic-settings create  `
+            --name  $Name `
+            --resource $Resource.ResourceId `
+            --metrics '[{""category"": ""AllMetrics"",""enabled"": true}]' `
+            --event-hub-rule /subscriptions/$Subscriptionid/resourceGroups/$resourceGroupname/providers/Microsoft.EventHub/namespaces/$Eventhub/authorizationrules/RootManageSharedAccessKey
     
         
         }
